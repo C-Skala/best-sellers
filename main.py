9b8a74679fc4a5e36d576c7ae0e934d63bd52583
C-Skala <cjskala@gmail.com>
2022-12-14 00:13:53 -0500

    first question done

--- main.py
+++ main.py
@@ -36,7 +36,10 @@ def example_analysis(book_list):
 
 def analysis_one(book_list):
     print("Analysis of which book had the lowest number of reviews in 2018")
-
+    books_2018 = list(filter(lambda book: book['year'] == 2018, book_list))
+    lowest_review_book = min(books_2018, key=lambda book: book['number_of_reviews'])
+    print(
+        f"The least reviewed book in 2018 was {lowest_review_book['name']} with reviews of {lowest_review_book['number_of_reviews']}")
 
 def analysis_two(book_list):
     print("Analysis of which genre (fiction or non-fiction) has appeared the most in the top 50's list")
